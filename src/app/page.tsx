41eadb2d8bcde6bfb8191fb9aa6cd960b84dbb5b
Matias Sallent <matias@reflexai.com>
2024-09-29 19:38:24 -0300

    Add scroll snap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Contact, Dev, Hero, Reviews, Showcase, Who } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-16 font-primary uppercase text-primary">
+    <main className="flex h-svh snap-y snap-mandatory flex-col gap-16 overflow-y-auto font-primary uppercase text-primary">
       <Hero />
       <Who />
       <Dev />
