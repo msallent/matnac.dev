667709c64f0d87946ce2e020063f02ffb7017626
Matias Sallent <matias@reflexai.com>
2024-09-29 00:27:30 -0300

    Add reviews

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -2,25 +2,34 @@ import { Carousel, CarouselItem } from "./Carousel";
 
 const REVIEWS = [
   {
-    date: "2/26/23",
-    author: "Todd Stuarts",
-    position: "Lead Engineer at MyCompany",
+    author: "Jeffrey Peltzman",
+    position: "Lead Designer at Formula.Monks",
     message:
-      "Matías Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Not even sure if I can fully describe how nice it has been to work with Matias since he joined our team. The product, UX, and design work we do is only as good as its implementation, and there was a long stretch pre-Matias where we consistently fell short. I have been designing and helping build for the web for a really long time and have rarely come across a dev with such attention for detail to the degree that Matias has. His work is impeccable.",
   },
   {
-    date: "2/26/23",
-    author: "Todd Stuarts",
-    position: "Lead Engineer at MyCompany",
+    author: "Sara Taillon",
+    position: "Managing Partner at Formula.Monks",
     message:
-      "Matías Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Matias is a super talented engineer who takes an interest in making things better, not just on the frontend, but in processes and ways of working within the team and beyond. He's one of the top FE engineers I've ever met. I'm grateful for all the work we've done together and I hope our paths cross again someday.",
   },
   {
-    date: "2/26/23",
-    author: "Todd Stuarts",
-    position: "Lead Engineer at MyCompany",
+    author: "Nicolas Ferrucci",
+    position: "Product Owner at TheoremOne",
     message:
-      "Matías Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Matias consistently brings a positive attitude and a solution-oriented mindset, making it easy to collaborate and innovate. I appreciate his ability to mentor others, and it's clear that his contributions have made a lasting impact on our product and team dynamics.",
+  },
+  {
+    author: "Simon Hunter",
+    position: "Director of Engineering at ReflexAI",
+    message:
+      "Matias' leadership around this release has been exceptional - he's been checking in on all the issues regularly and has a solid understanding of the status of everything at each moment. This has an enormous impact on the entire company.",
+  },
+  {
+    author: "Danielle Krieger",
+    position: "Software Engineer at MediaMonks",
+    message:
+      "Technically, he surpasses himself day by day. He's also very good at communicating, both internally and with clients. His English level is excellent and he's very correct and respectful. In my opinion, he has a lot of potential to be a lead.",
   },
 ];
 
@@ -41,14 +50,10 @@ export function Reviews() {
         <Carousel>
           {REVIEWS.map((review, index) => (
             <CarouselItem key={index}>
-              <div className="flex h-60 flex-col justify-between border-r border-secondary">
-                <p>{review.date}</p>
-
-                <span className="flex flex-col gap-2">
-                  <p>{review.author}</p>
-                  <p className="text-secondary">{review.position}</p>
-                  <p className="text-base normal-case">{review.message}</p>
-                </span>
+              <div className="flex h-60 flex-col justify-end gap-2 border-r border-secondary">
+                <p>{review.author}</p>
+                <p className="text-secondary">{review.position}</p>
+                <p className="text-base normal-case">{review.message}</p>
               </div>
             </CarouselItem>
           ))}
