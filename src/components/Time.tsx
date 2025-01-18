43317951346ce2f07451c096520400423f32879f
Matias Sallent <mqsallent@gmail.com>
2025-01-18 15:13:04 -0300

    Fix time component

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,30 +1,17 @@
 import { useEffect, useState } from "react";
+import { formatInTimeZone } from "date-fns-tz";
 
 const createDate = () => {
-  return new Date(new Date().toLocaleString("en-US", { timeZone: "America/Buenos_Aires" }));
+  return formatInTimeZone(new Date(), "America/Buenos_Aires", "HH:mm");
 };
 
 export function Time() {
   const [date, setDate] = useState(createDate);
 
-  const updateDate = (previous: Date) => {
-    const current = createDate();
-
-    if (current.getMinutes() !== previous.getMinutes()) {
-      return current;
-    }
-
-    return previous;
-  };
-
   useEffect(() => {
-    const interval = setInterval(() => setDate(updateDate), 1000);
+    const interval = setInterval(() => setDate(createDate), 1000);
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <span suppressHydrationWarning>
-      {`${date.getHours()}`.padStart(2, "0")}:{`${date.getMinutes()}`.padStart(2, "0")} (GMT-3)
-    </span>
-  );
+  return <span>{date} (GMT-3)</span>;
 }
